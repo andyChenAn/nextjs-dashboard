30c362fb4d4ca5a5c4cc4105ef940576a822c83a
andy <160149808@qq.com>
2024-09-10 21:37:36 +0800

    搭建nextjs项目

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter , Lusitana } from 'next/font/google';
+export const inter = Inter({subsets : ['latin']});
+export const inter2 = Inter({subsets : ['cyrillic-ext']});
+export const lusitana = Lusitana({subsets : ['latin'] , weight : '700'});
